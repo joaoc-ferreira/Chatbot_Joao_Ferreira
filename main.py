0b55737d863f01ada2e3be2aeadaef358b84c59b
joaoc-ferreira <jferreira93@hotmail.com>
2025-01-21 14:14:23 +0000

    Apelido
    Por fim pedi ao user que me escrevesse o seu nome de família.

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print(f"Seja bem-vindo/a {nome} !")
 idade: int = input("Por favor, diga-me a sua idade: ")
 print(f"Você tem {idade} anos de vida no planeta terra.")
 
+apelido: str = input("Qual é o seu nome de família? ")
+print(f"O seu nome completo é {nome} {apelido} e conta com {idade} Primaveras.")
