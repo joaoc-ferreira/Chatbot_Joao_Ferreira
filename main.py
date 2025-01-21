a6b3ed36d01d048debc1af47b1e85ddc9eec0c92
joaoc-ferreira <jferreira93@hotmail.com>
2025-01-21 14:13:28 +0000

    Anos
    Pedi ao user para introduzir a sua idade.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print("Olá mundo!")
 nome: str = input("Por favor, introduza o seu nome: ")
 print(f"Seja bem-vindo/a {nome} !")
+
+idade: int = input("Por favor, diga-me a sua idade: ")
+print(f"Você tem {idade} anos de vida no planeta terra.")
+
