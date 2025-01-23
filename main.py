de797c824fff2e65c963be995fbb41faa72da9bb
joaoc-ferreira <jferreira93@hotmail.com>
2025-01-23 18:03:28 +0000

    Delete main.py
    O antigo ficheiro main.py misturou-se com o da aula o que deu azo a confusão na sua leitura. Eliminei-o por isso.

--- main.py
+++
@@ -1,9 +0,0 @@
-print("Olá mundo!")
-nome: str = input("Por favor, introduza o seu nome: ")
-print(f"Seja bem-vindo/a {nome} !")
-
-idade: int = input("Por favor, diga-me a sua idade: ")
-print(f"Você tem {idade} anos de vida no planeta terra.")
-
-apelido: str = input("Qual é o seu nome de família? ")
-print(f"O seu nome completo é {nome} {apelido} e conta com {idade} Primaveras.")
